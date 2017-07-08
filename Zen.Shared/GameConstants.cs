8e8596505a6550eb0b268e593602daadac71a58e
William <talleurw@gmail.com>
2017-07-08 13:52:49 -0700

    Added GameConstants

---
+++ Zen.Shared/GameConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zen.Shared
+{
+    public class GameConstants
+    {
+        public const string Title = "Zen";
+        public const int Port = 43594;
+        public const int Version = 530;
+    }
+}
