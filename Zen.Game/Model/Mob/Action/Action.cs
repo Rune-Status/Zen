ffdc74bbdfe5a206d6933d39d88e875a68f01a63
William <talleurw@gmail.com>
2017-07-18 20:35:18 -0700

    Initial commit of action system

---
+++ Zen.Game/Model/Mob/Action/Action.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zen.Game.Model.Mob.Action
+{
+    public abstract class Action<T> where T : Mob
+    {
+
+        protected T Mob { get; set; }
+
+        protected Action(T mob)
+        {
+            this.Mob = mob;
+        }
+
+        protected abstract void Start();
+
+        protected abstract void Stop();
+
+        protected abstract bool IsCancellable();
+    }
+}
